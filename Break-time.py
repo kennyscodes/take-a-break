c8e96715c4c46faf01578cddc5deb20b465dab46
Kenny Gichuki <21562148+kennyscodes@users.noreply.github.com>
2021-04-26 16:27:08 +0300

    Add files via upload

--- Break-time.py
+++ Break-time.py
@@ -1,5 +1,11 @@
 import webbrowser
 import time
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=8wloBDrMKP4")
+total_breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime())
+while(break_count <total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=8wloBDrMKP4")
+    break_count = break_count + 1
