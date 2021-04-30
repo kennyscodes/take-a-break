d0ee9c859f55749c17989c3b84d51c30eaad63c8
Kenny Gichuki <21562148+kennyscodes@users.noreply.github.com>
2021-04-30 20:12:05 +0300

    Update Break-time.py

--- Break-time.py
+++ Break-time.py
@@ -5,6 +5,7 @@ total_breaks = 3
 break_count = 0
 
 print("This program started on "+time.ctime())
+
 while(break_count <total_breaks):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=8wloBDrMKP4")
